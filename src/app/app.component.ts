23dbda1fb9f514edb576ae57a76436e270698092
demitrimax <armandoaguilar1@yahoo.com>
2019-09-27 06:03:07 -0500

    primeras pruebas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'curso-angular Verificando';
+  title = 'Componente base principal ';
 }
---
+++ src/app/empleados/empleados.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'empleado',
+  templateUrl: './empleado.component.html'
+})
+
+export class EmpleadosComponent{
+  titulo = 'Titulo del componente empleado';
+
+
+}
